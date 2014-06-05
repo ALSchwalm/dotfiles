caabde257cc23c7a21b1ccef91c569ceca52d056
Adam Schwalm <adamschwalm@gmail.com>
2014-06-04 22:14:02 -0400

    company-dabbrev stop downcase

--- emacs.d/setup-autocomplete.el
+++ emacs.d/setup-autocomplete.el
@@ -1,11 +1,13 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (add-to-list 'company-backends 'company-c-headers)
-(setq company-global-modes '(c-mode c++-mode lisp-mode emacs-lisp-mode))
-(setq company-c-headers-path-system '("/usr/include/c++/4.9.0/"))
-(setq company-clang-arguments '("-std=c++11"))
-(setq company-dabbrev-code-time-limit 0)
-(setq company-idle-delay 0)
+(setq company-global-modes '(c-mode c++-mode lisp-mode emacs-lisp-mode)
+      company-c-headers-path-system '("/usr/include/c++/4.9.0/")
+      company-clang-arguments '("-std=c++11")
+      company-dabbrev-code-time-limit 0
+      company-idle-delay 0
+      company-dabbrev-downcase nil
+      company-dabbrev-ignore-case nil)
 
 ;; auto-complete setup
 (require 'auto-complete-config)
