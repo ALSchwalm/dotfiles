fe5ed1f00ad8cf6b235ff338f79738a9c8a21e78
Adam Schwalm <adamschwalm@gmail.com>
2014-11-07 21:55:23 -0600

    Paredit in slime repl

--- emacs.d/setup-slime.el
+++ emacs.d/setup-slime.el
@@ -6,4 +6,13 @@
 (require 'slime)
 (slime-setup '(slime-fancy))
 
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
 (provide 'setup-slime)
