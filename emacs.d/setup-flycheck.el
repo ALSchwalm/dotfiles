cb24b11cc1ae08559344dc3c3c28c1a2bac5c213
Adam Schwalm <adamschwalm@gmail.com>
2014-11-12 18:50:03 -0600

    Better flycheck prefix

--- emacs.d/setup-flycheck.el
+++ emacs.d/setup-flycheck.el
@@ -6,7 +6,8 @@
   (progn
     (setq flycheck-idle-change-delay 2
           flycheck-check-syntax-automatically '(save new-line mode-enabled)
-          flycheck-clang-include-path (quote ("/usr/include")))
+          flycheck-clang-include-path (quote ("/usr/include"))
+          flycheck-keymap-prefix (kbd "C-c f"))
 
     ;; Enable better c++
     (add-hook 'c++-mode-hook (lambda()
