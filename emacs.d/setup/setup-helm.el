0769fa415b5665823ef978dc63c48f2103fa24f2
Adam Schwalm <adamschwalm@gmail.com>
2018-08-21 22:45:46 -0500

    Use fuzzy match for helm-M-x

--- emacs.d/setup/setup-helm.el
+++ emacs.d/setup/setup-helm.el
@@ -1,11 +1,12 @@
 
 (req-package helm
   :config
-  (setq helm-quick-update                     t ; do not display invisible candidates
-        helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
-        helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+  (setq helm-quick-update t
+        helm-M-x-fuzzy-match t
+        helm-buffers-fuzzy-matching t
+        helm-scroll-amount 8
+        helm-candidate-number-limit 20
         helm-ff-file-name-history-use-recentf t)
-
   :bind (("C-c h" . helm-command-prefix)))
 
 (provide 'setup-helm)
